04a5f776761940553b97aeb58aea0f08271829b1
anyway3 <113296263+anyway3@users.noreply.github.com>
2025-06-23 15:24:29 +0900

    Add files via upload

--- TextRPGPlay/TextRPGPlay/Item.cpp
+++ TextRPGPlay/TextRPGPlay/Item.cpp
@@ -1,80 +1,196 @@
 #include "Item.h"
-#include <string>
-#include "Character.h"
-#include "Inventory.h"
-
-//=== Item 클래스 생성자 구현 ===//
-Item::Item(const std::string& name, int price, ItemType type)
-    : name(name), price(price), type(type) {
-}
-expendables::expendables(const std::string& name, int price, int value)
-    : Item(name, price, ItemType::expendables), value(value) {
-}
-
-
-//=== HealingPotion 클래스 구현 ===//
-Potion::Potion(const std::string& name, int price, int healAmount)
-    : expendables(name, price, healAmount) {
-}  // 상위 생성자에 넘김
-
-void Potion::Use(Character* character, Inventory* inventory) {
-    if (character) {
-        character->RecoveryHP(this->GetValue());  // expendables의 value 사용
-    }
-}
+#include<string>
+#include<iostream>
 
 
 
-//=== Weapon 클래스 구현 ===//
-Weapon::Weapon(const std::string& name, int price, int attackPower)
-    : Item(name, price, ItemType::Weapon), attackPower(attackPower), equip(false) {
+
+Item::Item(std::string name, ItemType type, int Value, int Atack, int Defense, int Heal, bool Equip)
+	:name(name), type(type),Value(Value), Atack(Atack), Defense(Defense), Heal(Heal), Equip(Equip) {}
+
+
+//장비 여부 가져오기
+bool Item::GetEquip()
+{
+	if (this->type == ItemType::Armor || this->type == ItemType::Weapon)
+	{
+		return this->Equip;
+	}
+	else
+	{
+		std::cout << "장비아이템이 아닙니다." << endl;
+		return false;
+	}
 }
 
-void Weapon::Use(Character* character, Inventory* inventory) {
-    if (inventory) {
-        // 캐릭터가 이 무기를 장착하도록 함
-        inventory->EquipWeapon(this);
-    }
+//장비 및 장비해제
+void Item::SetEquip()
+{
+	if (this->type == ItemType::Armor || this->type == ItemType::Weapon)
+	{
+		if (this->Equip == false)
+		{
+			this->Equip = true;
+		}
+		else
+		{
+			this->Equip = false;
+		}
+	}
+	else
+	{
+		std::cout << "장비아이템이 아닙니다." << endl;
+	}
+}
+
+//캐릭터 힐기능
+void Item::Use(Character* character)
+{
+	if (this->type == ItemType::Potion)
+	{
+		character->RecoveryHP(Heal);
+	}
+
+	else
+	{
+		std::cout << "회복아이템이 아닙니다" << endl;
+	}
+
+}
+
+//장비 업그레이드
+void Item::Upgrade()
+{
+	if(this->type == ItemType::Armor || this->type == ItemType::Weapon)
+	{
+		this->UpgradePhase++;
+
+		if (this->type == ItemType::Armor)
+		{
+			this->Defense = this->Defense + (10 * this->UpgradePhase);
+		}
+
+		else
+		{
+			this->Atack = this->Atack + (5 * this->UpgradePhase);
+		}
+
+	}
+	else
+	{
+		std::cout << "강화 가능 아이템이 아닙니다." << std::endl;
+	}
 }
 
 
 
-//=== Armor 클래스 구현 ===//
-Armor::Armor(const std::string& name, int price, int bonusHealth)
-    : Item(name, price, ItemType::Armor), health(bonusHealth), equip(false) {
-}
-
-void Armor::Use(Character* character, Inventory* inventory) {
-    if (inventory) {
-        // 캐릭터가 이 방어구를 장착하도록 함
-        inventory->EquipArmor(this);
-    }
-}
-
-void Armor::Upgrade() {
-    if (upgradePhase < 6) {
-        ++upgradePhase;
-        this->health +=5;
-    }
-}
-
-void Weapon::Upgrade() {
-    // 예시: 최대 5강까지 가능하며, 강화마다 공격력이 5씩 증가
-    if (upgradePhase < 5) {
-        ++upgradePhase;
-        this->attackPower +=5;
-    }
-}
 
 
-//복사생성자들 구현
-Item* Weapon::Clone() const {
-    return new Weapon(*this);
-}
 
-Item* Armor::Clone() const {  
-    return new Armor(*this);  
-}
-Item* Potion::Clone() const {
-    return new Potion(*this); // 복사 생성자 호출
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#include "Item.h"
+//#include <string>
+//#include "Character.h"
+//#include "Inventory.h"
+//
+////=== Item 클래스 생성자 구현 ===//
+//Item::Item(const std::string& name, int price, ItemType type)
+//    : name(name), price(price), type(type) {
+//}
+//
+////소모품 타입 생성자 구현
+//Expendable::Expendable(const std::string& name, int price, int value)
+//    : Item(name, price, ItemType::Expendable), value(value) {
+//}
+//
+//
+////=== HealingPotion 클래스 구현 ===//
+//Potion::Potion(const std::string& name, int price, int healAmount)
+//    : Expendable(name, price, healAmount) {
+//}  
+//
+//void Potion::Use(Character* character, Inventory* inventory) {
+//    if (character) {
+//        character->RecoveryHP(this->GetValue());  // Expendable의 value 사용
+//    }
+//}
+//
+//
+//
+////=== Weapon 클래스 구현 ===//
+//Weapon::Weapon(const std::string& name, int price, int attackPower)
+//    : Item(name, price, ItemType::Weapon), attackPower(attackPower), equip(false) {
+//}
+//
+//void Weapon::Use(Character* character, Inventory* inventory) {
+//    if (inventory) {
+//        // 캐릭터가 이 무기를 장착하도록 함
+//        inventory->EquipWeapon(this);
+//    }
+//}
+//
+//
+//
+////=== Armor 클래스 구현 ===//
+//Armor::Armor(const std::string& name, int price, int bonusHealth)
+//    : Item(name, price, ItemType::Armor), health(bonusHealth), equip(false) {
+//}
+//
+//void Armor::Use(Character* character, Inventory* inventory) {
+//    if (inventory) {
+//        // 캐릭터가 이 방어구를 장착하도록 함
+//        inventory->EquipArmor(this);
+//    }
+//}
+//
+//void Armor::Upgrade() {
+//    if (upgradePhase < 6) {
+//        ++upgradePhase;
+//        this->health += 5;
+//    }
+//}
+//
+//void Weapon::Upgrade() {
+//    // 예시: 최대 5강까지 가능하며, 강화마다 공격력이 5씩 증가
+//    if (upgradePhase < 5) {
+//        ++upgradePhase;
+//        this->attackPower += 5;
+//    }
+//}
+//
+//
+////복사생성자들 구현
+//Item* Weapon::Clone() const {
+//    return new Weapon(*this);
+//}
+//
+//Item* Armor::Clone() const {
+//    return new Armor(*this);
+//}
+//Item* Potion::Clone() const {
+//    return new Potion(*this); // 복사 생성자 호출
+//}
+//
--- TextRPGPlay/TextRPGPlay/Item.h
+++ TextRPGPlay/TextRPGPlay/Item.h
@@ -1,111 +1,180 @@
 #pragma once
-
-#include <string>
-
-class Character; // Forward declaration added
-class Inventory;
+#include<string>
+#include "Character.h"
 
 enum class ItemType {
-    expendables,
-    Weapon,
-    Armor 
+	Armor,
+	Weapon,
+	Expendables,
+	Potion
 };
 
-class Item{
-protected:
-    std::string name;
-    int price;
-    ItemType type;
-
-public:
-    Item(const std::string& name, int price, ItemType type) :name(name), price(price), type(type) {};
-    virtual void Use(Character* character, Inventory* inventory) = 0;
-    virtual ~Item() = default;
-    virtual Item* Clone() const = 0;
-
-    std::string GetName() const { return name; }
-    int GetPrice() const { return price; }
-    ItemType GetType() const { return type; }
-
-};
-
-
-class Weapon : public Item {
+class Item {
 private:
-    int upgradePhase = 0;
-    int attackPower;
-    bool equip;
-
+	std::string name;
+	ItemType type= ItemType::Expendables;
+	int Value;
+	int Atack=0;
+	int Defense=0;
+	int Heal=0;
+	int UpgradePhase = 0;
+	bool Equip=false;
 public:
-    Weapon(const std::string& name, int price, int attackPower);
-    void Use(Character* character, Inventory* inventory) override;
-    Item* Clone() const override;
+	Item(std::string name, ItemType type, int Value, int Atack, int Defense, int Heal, bool Equip);
+	//생성자
 
-    int GetupgradePhase() const { return upgradePhase; }
-    //강화 횟수 가져오기
+	std::string GetName() { return name; }
+	//이름 가져오기
 
-    int GetAttackPower() const { return attackPower; }
-    //공격력 가져오기
+	ItemType GetType() { return type; }
+	//타입 가져오기
 
-    bool IsEquipped() const { return equip; }
-    //착용된 아이템인지 확인
+	int GetAtack() { return Atack; }
+	//공격력 가져오기
 
-    void SetEquipped(bool equipped) { equip = equipped; }
-    //착용 상태 변경
+	int GetDefense() { return Defense; }
+	//방어력 가져오기
 
-    void Upgrade();
-    //강화 기능
-       //강화 기능 5강까지 가능
+	int GetHeal() { return Heal; }
+	//회복량 가져오기
 
+	bool GetEquip();
+	//장비 여부 가져오기
+
+	void SetEquip();
+	//장비및 장비 해제
+
+	int GetUpgradePhase() { return UpgradePhase; }
+	//강화성공 횟수 가져오기
+
+	void Use(Character* character);
+	//캐릭터 치료기능
+
+	void Upgrade();
+	//업그레이드 기능 구현
 };
 
-class Armor : public Item {
-private:
-    int upgradePhase = 0;
-    int health;
-    bool equip;
-
-public:
-    Armor(const std::string& name, int price, int health);
-    void Use(Character* character, Inventory* inventory) override;
-    Item* Clone() const override;
-
-    int GetupgradePhase() const { return upgradePhase; }
-    //강화 횟수 가져오기
 
 
-    int GetBonusHealth() const { return health; }
-    //방어력 가져오기
 
-    bool IsEquipped() const { return equip; }
-    //착용된 아이템인지 확인
 
-    void SetEquipped(bool equipped) { equip = equipped; }
-    //착용 상태 변경
 
-    void Upgrade();
-    //강화 기능 5강까지 가능
 
-};
 
-class expendables : public Item {
-protected:
-    int value;
 
-public:
-    expendables(const std::string& name, int price, int value);
 
-    virtual void Use(Character* character, Inventory* inventory) = 0;
-    virtual Item* Clone() const = 0;
 
-    int GetValue() const { return value; }
-};
 
-class Potion : public expendables {
-
-public:
-    Potion(const std::string& name, int price, int value);
-    void Use(Character* character, Inventory* inventory) override;
-    Item* Clone() const override;
-
-};
+//#pragma once
+//
+//#include <string>
+//#include "Character.h"
+//#include "Inventory.h"
+//
+//class Character; // Forward declaration added
+//class Inventory;
+//
+//enum class ItemType
+//{
+//    Expendable,
+//    Weapon,
+//    Armor
+//};
+//
+//class Item
+//{
+//protected:
+//    std::string name;
+//    int price;
+//    ItemType type;
+//    bool equip=false;
+//
+//public:
+//    Item(const std::string& name, int price, ItemType type) :name(name), price(price), type(type) {};
+//    virtual void Use(Character* character, Inventory* inventory=nullptr) = 0;
+//    virtual ~Item() = default;
+//    virtual Item* Clone() const = 0;
+//
+//    std::string GetName() const { return name; }
+//    int GetPrice() const { return price; }
+//    ItemType GetType() const { return type; }
+//    bool IsEquipped() const { return equip; }
+//    void SetEquipped(bool equipped) { equip = equipped; }
+//
+//};
+//
+//
+//class Weapon : public Item
+//{
+//private:
+//    int upgradePhase = 0;
+//    int attackPower;
+//
+//public:
+//    Weapon(const std::string& name, int price, int attackPower);
+//    void Use(Character* character, Inventory* inventory) override;
+//    Item* Clone() const override;
+//    int GetupgradePhase() const { return upgradePhase; }
+//    //강화 횟수 가져오기
+//
+//    int GetAttackPower() const { return attackPower; }
+//    //공격력 가져오기
+//
+//
+//
+//    void Upgrade();
+//    //강화 기능
+//        //강화 기능 5강까지 가능
+//
+//};
+//
+//class Armor : public Item
+//{
+//private:
+//    int upgradePhase = 0;
+//    int health;
+//    bool equip;
+//
+//public:
+//    Armor(const std::string& name, int price, int health);
+//    void Use(Character* character, Inventory* inventory) override;
+//    Item* Clone() const override;
+//
+//    int GetupgradePhase() const { return upgradePhase; }
+//    //강화 횟수 가져오기
+//
+//
+//    int GetBonusHealth() const { return health; }
+//    //방어력 가져오기
+//
+//
+//    void Upgrade();
+//    //강화 기능 5강까지 가능d
+//
+//};
+//
+//class Expendable : public Item
+//{
+//protected:
+//    int value;
+//
+//public:
+//    Expendable(const std::string& name, int price, int value);
+//
+//    // 여기를 수정합니다: `=nullptr` 기본값을 제거합니다!
+//    virtual void Use(Character* character, Inventory* inventory) = 0;
+//    virtual Item* Clone() const = 0;
+//
+//    int GetValue() const { return value; }
+//};
+//
+//
+//class Potion : public Expendable
+//{
+//
+//public:
+//    Potion(const std::string& name, int price, int value);
+//    void Use(Character* character, Inventory* inventory) override;
+//    Item* Clone() const override;
+//
+//};
