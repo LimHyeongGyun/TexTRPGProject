7a82e41418b6c32a30516d65e45b15a63e846bde
LimHyeonGyun <200hk@naver.com>
2025-06-24 17:08:05 +0900

    feature
    equip을 public으로 변경

--- TextRPGPlay/TextRPGPlay/Inventory.h
+++ TextRPGPlay/TextRPGPlay/Inventory.h
@@ -17,8 +17,6 @@ private:
 	void DisplayArmor();
 
 	//아이템 사용
-	void EquipWeapon(Item* weapon); //무기 장착
-	void EquipArmor(Item* armor); //방어구 장착
 	void UnEquipWeapon(); //방어구 해제
 	void UnEquipArmor(); //무기 해제
 public:
@@ -28,6 +26,9 @@ public:
 	std::unordered_map<Item*, int> otherItems;
 	std::vector<Item*> weaponItems;
 	std::vector<Item*> armorItems;
+
+	void EquipWeapon(Item* weapon); //무기 장착
+	void EquipArmor(Item* armor); //방어구 장착
 
 	//인벤토리 관리
 	void ClassificationItem(std::vector<Item*> items); //획득한 아이템을 분류해서 인벤토리에 넣기
