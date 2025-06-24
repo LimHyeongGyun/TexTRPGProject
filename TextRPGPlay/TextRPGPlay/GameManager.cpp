802f08185a14f6a8fff3dd6bff35f42c6b705b2f
Manwol814 <dofr1564@naver.com>
2025-06-24 17:04:19 +0900

    fix 몬스터들 생성자로 변경

--- TextRPGPlay/TextRPGPlay/GameManager.cpp
+++ TextRPGPlay/TextRPGPlay/GameManager.cpp
@@ -1,5 +1,4 @@
 #include "GameManager.h"
-#include "Monster.h"
 #include "Goblin.h"
 #include "Orc.h"
 #include "Troll.h"
@@ -17,59 +16,21 @@ using namespace std;
 
 GameManager::GameManager() {}
 
-Monster* GameManager::GenerateMonster(int level) {
-    if (level <= 3) return new Slime(level);
-    else if (level <= 5) return new Orc(level);
-    else if (level <= 7) return new Troll(level);
-    else return new Goblin(level);
+Slime GenerateSlime(int level) {
+    return Slime(level);
 }
-
-Monster* GameManager::GenerateBossMonster(int level) {
-    return new Dragon(level);
+Orc GenerateOrc(int level) {
+    return Orc(level);
 }
-
-void GameManager::Battle(Character* character, Monster* monster) {
-    while (character->IsAlive() && monster->IsAlive()) {
-        int damage = character->Attack();
-        monster->takeDamage(damage);
-        cout << character->GetName() << "이(가) " << monster->getName()
-            << "을(를) 공격! 몬스터 체력: " << monster->getHealth() << endl;
-
-        if (monster->IsAlive()) {
-            int mDamage = monster->Attack();
-            character->TakeDamage(mDamage);
-            cout << monster->getName() << "의 반격! " << character->GetName()
-                << " 체력: " << character->GetCurrentHealth() << endl;
-        }
-    }
-
-    if (character->IsAlive()) {
-        cout << "\n전투 승리!" << endl;
-
-        int exp = monster->getExpDrop();
-        int gold = monster->getGoldDrop();
-        character->GetExperience(exp);
-        character->BorrowGold(gold);
-        cout << "경험치: +" << exp << ", 골드: +" << gold << endl;
-
-        ItemType dropType = monster->getItemDrop();
-        string dropName = monster->getItemDropName();
-
-        Item* dropItem = ItemManager::Get().CreateItem(dropName);
-        if (dropItem) {
-            vector<Item*> dropItems = { dropItem };
-            character->GetItem(dropItems);
-            cout << "아이템 획득: " << dropItem->GetName() << endl;
-        }
-    }
-    else {
-        cout << character->GetName() << "이(가) 사망했습니다. 게임 오버." << endl;
-    }
-
-    delete monster;
+Troll GenerateTroll(int level) {
+    return Troll(level);
 }
-
-void GameManager::DisplayInventory() {}
+Goblin GenerateGoblin(int level) {
+    return Goblin(level);
+}
+Dragon GenerateDragon(int level) {
+    return Dragon(level);
+}
 
 void GameManager::Run() {
     Character& player = *Character::Get();
@@ -79,11 +40,18 @@ void GameManager::Run() {
         player.DisplayStatus();
 
         cout << "\n===== 전투 시작 (레벨: " << player.GetLevel() << ") =====" << endl;
-        Monster* monster = GenerateMonster(player.GetLevel());
-        cout << monster->getName() << " 등장! 체력: " << monster->getHealth()
-            << ", 공격력: " << monster->Attack() << endl;
 
-        Battle(&player, monster);
+        int level = player.GetLevel();
+        Monster monster;
+        if (level <= 3) monster = GenerateSlime(level);
+        else if (level <= 5) monster = GenerateOrc(level);
+        else if (level <= 7) monster = GenerateTroll(level);
+        else monster = GenerateGoblin(level);
+
+        cout << monster.getName() << " 등장! 체력: " << monster.getHealth()
+            << ", 공격력: " << monster.Attack() << endl;
+
+        Battle(player, monster);
 
         if (!player.IsAlive()) break;
     }
@@ -92,12 +60,12 @@ void GameManager::Run() {
         cout << "\n===== 보스 몬스터 등장 =====" << endl;
         player.DisplayStatus();
 
-        Monster* boss = GenerateBossMonster(player.GetLevel());
-        cout << boss->getName() << " 등장! 체력: " << boss->getHealth()
-            << ", 공격력: " << boss->Attack() << endl;
+        Monster boss = GenerateDragon(player.GetLevel());
 
-        Battle(&player, boss);
-        delete boss;
+        cout << boss.getName() << " 등장! 체력: " << boss.getHealth()
+            << ", 공격력: " << boss.Attack() << endl;
+
+        Battle(player, boss);
 
         if (player.IsAlive()) {
             cout << "\n축하합니다! 보스를 물리치고 게임을 클리어했습니다!" << endl;
@@ -109,4 +77,41 @@ void GameManager::Run() {
 
     cout << "\n게임 종료. 메모리 정리 중..." << endl;
     delete Character::Get();
+}
+
+void GameManager::Battle(Character& player, Monster& monster) {
+    while (player.IsAlive() && monster.IsAlive()) {
+        int damage = player.Attack();
+        monster.takeDamage(damage);
+        cout << player.GetName() << "이(가) " << monster.getName()
+            << "을(를) 공격! 몬스터 체력: " << monster.getHealth() << endl;
+
+        if (monster.IsAlive()) {
+            int mDamage = monster.Attack();
+            player.TakeDamage(mDamage);
+            cout << monster.getName() << "의 반격! " << player.GetName()
+                << " 체력: " << player.GetCurrentHealth() << endl;
+        }
+    }
+
+    if (player.IsAlive()) {
+        cout << "\n전투 승리!" << endl;
+
+        int exp = monster.getExpDrop();
+        int gold = monster.getGoldDrop();
+        player.GetExperience(exp);
+        player.BorrowGold(gold);
+        cout << "경험치: +" << exp << ", 골드: +" << gold << endl;
+
+        string dropName = monster.getItemDropName();
+        Item* dropItem = ItemManager::Get().CreateItem(dropName);
+        if (dropItem) {
+            vector<Item*> dropItems = { dropItem };
+            player.GetItem(dropItems);
+            cout << "아이템 획득: " << dropItem->GetName() << endl;
+        }
+    }
+    else {
+        cout << player.GetName() << "이(가) 사망했습니다. 게임 오버." << endl;
+    }
 }
