b1fb81513c71334cb6f30dc01159ebc43ac5715a
anyway3 <tangle2002@naver.com>
2025-06-25 21:02:10 +0900

    inventory 345번줄 and를 or 로 변경

--- TextRPGPlay/TextRPGPlay/Inventory.cpp
+++ TextRPGPlay/TextRPGPlay/Inventory.cpp
@@ -342,7 +342,7 @@ void Inventory::DIsplayOtherItem(string func)
                 }
             }
             //찾아봐도 소지하고 있지 않는 아이템 이름일 때
-            if (key != item.first->GetName() && expendableItems.find(item.first) == otherItems.end())
+            if (key != item.first->GetName() || expendableItems.find(item.first) == otherItems.end())
             {
                 cout << GameManager::Get().WrongInputMessage();
                 DIsplayOtherItem(func);
